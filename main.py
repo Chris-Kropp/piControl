17774dbcab36fa25800a78ce9d8b8f449e317330
Chris <chrisk844@gmail.com>
2021-07-02 02:27:52 -0600

    Added light controls, temporary alarm controls

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from phue import Bridge
+import webbrowser
 
 class Main:
     def __init__(self):
+        self.b = Bridge("192.168.0.20")
+        self.chair_on = self.b.get_light(1, "on")
+        self.chair_bri = self.b.get_light(1, "bri")
+        self.desk_on = self.b.get_light(2, "on")
+        self.desk_bri = self.b.get_light(2, "bri")
         self.window = Tk()
         # self.window.attributes('-type', 'dock')
         # self.window.attributes('-fullscreen', True)
@@ -16,6 +23,7 @@ class Main:
         self.settings_img = PhotoImage(file="settings.png")
         self.alarm_img = PhotoImage(file="alarm.png")
         self.stock_img = PhotoImage(file="stocks.png")
+        self.power_img = PhotoImage(file="power.png")
         self.loadMain()
 
         mainloop()
@@ -47,7 +55,7 @@ class Main:
 
         stockFrame = Frame(frame_two, width=100, height=100)
         stockFrame.grid_propagate(False)
-        Button(stockFrame, text="Alarm", image=self.alarm_img, compound=TOP).grid(sticky="wens")
+        Button(stockFrame, text="Alarm", image=self.alarm_img, compound=TOP, command=self.open_alarm).grid(sticky="wens")
         stockFrame.columnconfigure(0, weight=1)
         stockFrame.rowconfigure(0, weight=1)
         stockFrame.pack(fill="both", expand=True)
@@ -81,31 +89,56 @@ class Main:
             item.pack_forget()
 
         frame_back = Frame(self.mainFrame, height=10, background="")
-        Button(frame_back, compound=TOP, command=self.loadMain).grid(sticky="wens")
+        Button(frame_back, text="<", compound=TOP, command=self.loadMain).grid(sticky="wens")
 
-        frame_back.pack(side="top", fill="x", expand=False)
-
-        # frame_back = Frame(self.mainFrame)
-        # frame_back.pack(side="top", fill="both", expand=True)
+        frame_back.pack(side="top", fill="both", expand=False)
 
         frame_one = Frame(self.mainFrame)
         frame_one.pack(side="left", fill="both", expand=True)
 
         lightFrame = Frame(frame_one, width=100, height=100)
         lightFrame.grid_propagate(False)
-        Button(lightFrame, text="Lights", image=self.light_img, compound=TOP, command=self.loadLights).grid(sticky="wens")
+        self.chair_scale = Scale(lightFrame, from_=254, to=30, length=400, showvalue=0, resolution=10, command=self.chair_light_brightness)
+        self.chair_scale.pack(side=LEFT)
+        self.chair_scale.set(self.chair_bri)
+        Button(lightFrame, text="Chair", image=self.power_img, compound=TOP, command=self.toggle_chair).grid(padx=(25, 0), sticky="WENS")
         lightFrame.columnconfigure(0, weight=1)
         lightFrame.rowconfigure(0, weight=1)
-        lightFrame.pack(fill="both", expand=True)
+        lightFrame.pack(side=LEFT, fill="both", expand=True)
 
-        settingsFrame = Frame(frame_one, width=100, height=100)
-        settingsFrame.grid_propagate(False)
-        Button(settingsFrame, text="Settings", image=self.settings_img, compound=TOP).grid(sticky="wens")
-        settingsFrame.columnconfigure(0, weight=1)
-        settingsFrame.rowconfigure(0, weight=1)
-        settingsFrame.pack(fill="both", expand=True)
+        lightFrameTwo = Frame(frame_one, width=100, height=100)
+        lightFrameTwo.grid_propagate(False)
+        self.desk_scale = Scale(lightFrameTwo, from_=254, to=30, length=400, showvalue=0, resolution=10, command=self.desk_light_brightness)
+        self.desk_scale.pack(side=RIGHT)
+        self.desk_scale.set(self.desk_bri)
+        Button(lightFrameTwo, text="Desk", image=self.power_img, compound=TOP, command=self.toggle_desk).grid(padx=(0, 25), sticky="WENS")
+        lightFrameTwo.columnconfigure(0, weight=1)
+        lightFrameTwo.rowconfigure(0, weight=1)
+        lightFrameTwo.pack(expand=True, fill="both", side=RIGHT)
 
-        frame_two = Frame(self.mainFrame)
-        frame_two.pack(side="left", fill="both", expand=True)
+    def toggle_chair(self):
+        self.chair_on = not self.chair_on
+        self.b.set_light(1, 'on', self.chair_on)
+
+    def toggle_desk(self):
+        self.desk_on = not self.desk_on
+        self.b.set_light(2, 'on', self.desk_on)
+
+    def chair_light_brightness(self, brightness):
+        if(self.chair_on):
+            self.b.set_light(1, 'bri', int(brightness))
+            self.chair_bri = int(brightness)
+        else:
+            self.chair_scale.set(self.chair_bri)
+
+    def desk_light_brightness(self, brightness):
+        if(self.desk_on):
+            self.b.set_light(2, 'bri', int(brightness))
+            self.desk_bri = int(brightness)
+        else:
+            self.desk_scale.set(self.desk_bri)
+
+    def open_alarm(self):   #TODO: replace with function that gets and sends data to api instead of opening web interface
+        webbrowser.open("192.168.0.45:8080")
 
 Main()
